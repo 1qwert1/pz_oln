c54795f8c2451e0f0c3271890a37340dbab863d4
nemo <vzabudko@gmail.com>
2016-03-03 23:21:49 +0200

    подготовил пустіе страницыдля печати

--- obrabotka/list.php
+++ obrabotka/list.php
@@ -7,13 +7,16 @@ echo "<pre>";
 print_r($_POST);
 echo "</pre>";
 
+
+echo $_SESSION['$val'];
+
+echo "<pre>";
+var_dump($_SESSION);
+echo "</pre>";
+
 include '../for_form/header.php';
 
-//echo $_SESSION['$val'];
 
-//echo "<pre>";
-//var_dump($_SESSION);
-//echo "</pre>";
 
 echo '<button onclick=print() class="butt">До друку</button>';
 
